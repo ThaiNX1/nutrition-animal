06c39f7be9eae87bf111b0cc9352c59ffe7e0fb9
ThaiNX1 <nxthai22@gmail.com>
2022-10-31 22:41:56 +0700

    update

--- src/app/modules/account/account-info/account-info.component.ts
+++ src/app/modules/account/account-info/account-info.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserEntity, UsersService} from "../../../services";
+import {CookieService} from "ngx-cookie-service";
+import {GlobalVariable} from "../../../common/global-variable";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-account-info',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-info.component.scss']
 })
 export class AccountInfoComponent implements OnInit {
+  user?: UserEntity
+  isChangePassword = false
+  changePasswordForm!: FormGroup
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private cookie: CookieService,
+    private globalVariable: GlobalVariable,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.changePasswordForm = new FormGroup({
+      oldPassword: new FormControl(),
+      newPassword: new FormControl(),
+    })
+    const userCookie = this.cookie.get('tomeo_user');
+    const user = userCookie ? JSON.parse(userCookie) : {};
+    this.globalVariable.setIsLoading(true)
+    UsersService.getOneBase({
+      id: user.sub,
+      join: ['role', 'wallet'],
+    }).then((res) => {
+      this.globalVariable.setIsLoading(false)
+      this.user = res
+    }).catch((err) => {
+      this.globalVariable.setIsLoading(false)
+    })
+  }
+
+  submitForm(): void {
+
+  }
 }
--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountInfoComponent } from './account-info/account-info.component';
 import { DonateComponent } from './donate/donate.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 
 
@@ -11,7 +15,11 @@ import { DonateComponent } from './donate/donate.component';
     DonateComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzFormModule,
+    NzButtonModule
   ]
 })
 export class AccountModule { }
