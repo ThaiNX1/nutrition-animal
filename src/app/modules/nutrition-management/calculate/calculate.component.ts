f8f7cbd500f9c871becd9cf5e264f18d89251cb1
ThaiNX <thai.nx@sharitekvn.com>
2022-11-02 11:54:25 +0700

    update

--- src/app/modules/nutrition-management/calculate/calculate.component.ts
+++ src/app/modules/nutrition-management/calculate/calculate.component.ts
@@ -4,6 +4,7 @@ import { CommonService } from '../../../common/common.service';
 import { Router } from '@angular/router';
 import {
   CalculateRequestDto,
+  CalculateResponseDto,
   EnumCalculateRequestDtoAnimalType,
   GetManyIngredientEntityResponseDto,
   IngredientCalculateService,
@@ -49,6 +50,7 @@ export class CalculateComponent implements OnInit {
   totalPercentage = 0;
   totalPrice = 0;
   totalWeight = 0;
+  isPrint = true;
 
   constructor(
     private globalVariant: GlobalVariable,
@@ -176,182 +178,36 @@ export class CalculateComponent implements OnInit {
       animalType: this.infoForm.value.animal,
       ingredients,
     };
-    const abcs = {
-      DryMatter: {
-        code: 'DryMatter',
-        name: 'Vật chất khô',
-        unit: '%',
-        value: 92.05641135191306,
-      },
-      ME: {
-        code: 'ME',
-        name: 'ME, Heo',
-        unit: 'Kcal/kg',
-        value: 3449.29351963141,
-      },
-      CrudeProtein: {
-        code: 'CrudeProtein',
-        name: 'Đạm thô',
-        unit: '%',
-        value: 19.36974338384775,
-      },
-      Fat: {
-        code: 'CrudeFat',
-        name: 'Béo',
-        unit: '%',
-        value: 2.6273461274611645,
-      },
-      CrudeFiber: {
-        code: 'CrudeFiber',
-        name: 'Xơ thô',
-        unit: '%',
-        value: 1.730215398929058,
-      },
-      LysineTotal: {
-        code: 'CrudeFiber',
-        name: 'Lysine, Total',
-        unit: '%',
-        value: 1.1510256429513295,
-      },
-      MethionineTotal: {
-        code: 'MethionineTotal',
-        name: 'Methionine, Total',
-        unit: '%',
-        value: 0.36999999712675047,
-      },
-      MetCysTotal: {
-        code: 'MetCysTotal',
-        name: 'Met + Cys, Total',
-        unit: '%',
-        value: 0.71205129302465,
-      },
-      ThreonineTotal: {
-        code: 'ThreonineTotal',
-        name: 'Threonine, Total',
-        unit: '%',
-        value: 0.7974358930801734,
-      },
-      TryptophanTotal: {
-        code: 'TryptophanTotal',
-        name: 'Tryptophan, Total',
-        unit: '%',
-        value: 0.24564102941598648,
-      },
-      Lactose: {
-        code: 'Lactose',
-        name: 'Lactose',
-        unit: '%',
-        value: 7.17948717948718,
-      },
-      LYS_TH: {
-        code: 'LYS_TH',
-        name: 'LYS, TH Heo',
-        unit: '%',
-        value: 1.0225461228535726,
-      },
-      MET_TH: {
-        code: 'MET_TH',
-        name: 'MET, TH Heo',
-        unit: '%',
-        value: 0.3353846149566846,
-      },
-      MC_TH: {
-        code: 'MC_TH',
-        name: 'M+C, TH Heo',
-        unit: '%',
-        value: 0.6255769454515897,
-      },
-      THR_TH: {
-        code: 'THR_TH',
-        name: 'THR, TH Heo',
-        unit: '%',
-        value: 0.6940256200539761,
-      },
-      TRP_TH: {
-        code: 'TRP_TH',
-        name: 'TRP, TH Heo',
-        unit: '%',
-        value: 0.21641025959681243,
-      },
-      Calcium: {
-        code: 'Calcium',
-        name: 'Calcium (Ca)',
-        unit: '%',
-        value: 0.2623307690597497,
-      },
-      PhosphorusTotal: {
-        code: 'PhosphorusTotal',
-        name: 'Phosphorus, Total',
-        unit: '%',
-        value: 0.4099897360190367,
-      },
-      PhosphorusAvail: {
-        code: 'PhosphorusAvail',
-        name: 'Phosphorus, availability',
-        unit: '%',
-        value: 0.16351281794217917,
-      },
-      Sodium: {
-        code: 'Sodium',
-        name: 'Sodium (Na)',
-        unit: '%',
-        value: 0.02059743543848013,
-      },
-      Chloride: {
-        code: 'Chloride',
-        name: 'Chloride (Cl)',
-        unit: '%',
-        value: 0.04054615360039931,
-      },
-      Salt: {
-        code: 'Salt',
-        name: 'Muối (NaCl)',
-        unit: '%',
-        value: 0,
-      },
-      LinoleicAcid: {
-        code: 'LinoleicAcid',
-        name: 'Linoleic Acid',
-        unit: '%',
-        value: 0.6978794764249753,
-      },
-    };
-    Object.keys(abcs).map((key) => {
-      // @ts-ignore
-      this.itemCalculates.push(
-        new FormGroup({
+    this.globalVariant.setIsLoading(true);
+    IngredientCalculateService.ingredientCalculateControllerCalculateNutrition({
+      body: request,
+    })
+      .then((res: CalculateResponseDto) => {
+        this.globalVariant.setIsLoading(false);
+        Object.keys(res).map((key) => {
           // @ts-ignore
-          name: new FormControl(abcs[key].code),
-          // @ts-ignore
-          value: new FormControl(
-            // @ts-ignore
-            formatNumber(abcs[key].value, 'en-us', '1.0-2')
-          ),
-          // @ts-ignore
-          unit: new FormControl(abcs[key].unit),
-          isEdit: new FormControl(false),
-        })
-      );
-    });
-    this.getTotalPercentage();
-    // IngredientCalculateService.ingredientCalculateControllerCalculateNutrition({
-    //   body: request,
-    // })
-    //   .then((res) => {
-    //     Object.keys(res)?.map((key: string) => {
-    //       this.itemCalculates.push(
-    //         new FormGroup({
-    //           // @ts-ignore
-    //           name: new FormControl(res[key]),
-    //           // @ts-ignore
-    //           value: new FormControl(res[key]),
-    //           // @ts-ignore
-    //           unit: new FormControl(res[key]),
-    //         })
-    //       );
-    //     });
-    //   })
-    //   .catch((err) => {});
+          this.itemCalculates.push(
+            new FormGroup({
+              // @ts-ignore
+              name: new FormControl(res[key].code),
+              // @ts-ignore
+              value: new FormControl(
+                // @ts-ignore
+                formatNumber(res[key].value, 'en-us', '1.0-2')
+              ),
+              // @ts-ignore
+              unit: new FormControl(res[key].unit),
+              isEdit: new FormControl(false),
+            })
+          );
+        });
+        this.getTotalPercentage();
+        this.isPrint = true;
+      })
+      .catch((err) => {
+        this.globalVariant.setIsLoading(false);
+        this.itemCalculates.reset();
+      });
   }
 
   onPrint(): void {
@@ -462,9 +318,9 @@ export class CalculateComponent implements OnInit {
     // @ts-ignore
     windowPrt.focus();
     // @ts-ignore
-    // windowPrt.print();
+    windowPrt.print();
     // @ts-ignore
-    // windowPrt.close();
+    windowPrt.close();
   }
 
   onEdit(index: number): void {
